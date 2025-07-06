63ae9fabefccc6d8adfe923b40db28b22bb9919f
Lalith-Kumar11 <officiallalithkumar@gmail.com>
2025-07-06 19:57:29 +0530

    Add files via upload

---
+++ Week 3/LibraryManagement/src/main/java/com/library/main/MainApp.java
@@ -0,0 +1,22 @@
+package com.library.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.service.BookService;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // Load Spring context
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Get BookService bean
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Use BookService
+        bookService.addBook("Spring Framework Essentials");
+
+        // Close context
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+}
---
+++ Week 3/orm-learn/src/main/java/com/cognizant/ormlearn/model/Employee.java
@@ -0,0 +1,32 @@
+package com.cognizant.ormlearn.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+    
+}
